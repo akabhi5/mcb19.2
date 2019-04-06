0d34b53204ab4ac169f139284aa9965f4db68f9b
Abhishek Kumar <abhishekk580@gmail.com>
2019-04-06 22:36:37 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-
-import sys
-from cx_Freeze import setup, Executable
-
-setup(
-    name = "multiclipboard",
-    version = "19.2",
-    description = "multiclipboard by abhi",
-    executables = [Executable("MainWindow.py", icon="mainlogo.ico", base = "Win32GUI")])
